2d07093eed9d6e6bc3ecc8a4c1d9205c0e21eff3
Eric Drobotij <eric@drobotij.com>
2016-12-24 16:36:50 +0000

    Fixed controller indents. Added TeamController.

--- app/Http/Controllers/LeagueController.php
+++ app/Http/Controllers/LeagueController.php
@@ -6,94 +6,94 @@ use Illuminate\Http\Request;
 
 class LeagueController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+      //
+  }
 
-    public function index()
-    {
-      $leagues = app('db')->select("SELECT * FROM leagues");
+  public function index()
+  {
+    $leagues = app('db')->select("SELECT * FROM leagues");
 
-      return response()->json($leagues);
-    }
+    return response()->json($leagues);
+  }
 
-    public function get($slug)
-    {
-      $league = app('db')->select(
-        "SELECT l.name, l.description, l.city, l.state,
-                u.first_name, u.last_name, u.email
-         FROM leagues l
-         INNER JOIN users u
-         ON l.commissioner_id = u.id
-         WHERE slug = ?",
-        [$slug]
-      )[0];
+  public function get($slug)
+  {
+    $league = app('db')->select(
+      "SELECT l.name, l.description, l.city, l.state,
+              u.first_name, u.last_name, u.email
+       FROM leagues l
+       INNER JOIN users u
+       ON l.commissioner_id = u.id
+       WHERE slug = ?",
+      [$slug]
+    )[0];
 
-      return response()->json($league);
-    }
+    return response()->json($league);
+  }
 
-    public function store(Request $request)
-    {
-      app('db')->insert(
-        "INSERT INTO leagues (name, description, city, state, commissioner_id
-          created_at, updated_at, slug)
-         VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-        [
-          $request->input('name'),
-          $request->input('description'),
-          $request->input('city'),
-          $request->input('state'),
-          $request->input('commissioner_id'),
-          time(),
-          time(),
-          app('helper')->slugify($request->input('name'))
-        ]
-      );
-    }
+  public function store(Request $request)
+  {
+    app('db')->insert(
+      "INSERT INTO leagues (name, description, city, state, commissioner_id
+        created_at, updated_at, slug)
+       VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        $request->input('name'),
+        $request->input('description'),
+        $request->input('city'),
+        $request->input('state'),
+        $request->input('commissioner_id'),
+        time(),
+        time(),
+        app('helper')->slugify($request->input('name'))
+      ]
+    );
+  }
 
-    public function edit($id)
-    {
-      $league = app('db')->select(
-        "SELECT id, name, description, city, state FROM leagues WHERE id = ?",
-        [$id]
-      )[0];
+  public function edit($id)
+  {
+    $league = app('db')->select(
+      "SELECT id, name, description, city, state FROM leagues WHERE id = ?",
+      [$id]
+    )[0];
 
-      return response()->json($league);
-    }
+    return response()->json($league);
+  }
 
-    public function update(Request $request)
-    {
-      app('db')->update(
-        "UPDATE leagues
-          SET name = :name, description = :description, city = :city,
-            state = :state
-          WHERE id = :id",
-        [
-          'id' => $request->input('id'),
-          'name' => $request->input('name'),
-          'description' => $request->input('description'),
-          'city' => $request->input('city'),
-          'state' => $request->input('state')
-        ]
-      );
-    }
+  public function update(Request $request)
+  {
+    app('db')->update(
+      "UPDATE leagues
+        SET name = :name, description = :description, city = :city,
+          state = :state
+        WHERE id = :id",
+      [
+        'id' => $request->input('id'),
+        'name' => $request->input('name'),
+        'description' => $request->input('description'),
+        'city' => $request->input('city'),
+        'state' => $request->input('state')
+      ]
+    );
+  }
 
-    public function teams($slug)
-    {
-      $teams = app('db')->select(
-        "SELECT t.name, t.city, t.state FROM teams t
-         INNER JOIN leagues l
-         ON l.id = t.league_id
-         WHERE l.slug = ?",
-        [$slug]
-      );
+  public function teams($slug)
+  {
+    $teams = app('db')->select(
+      "SELECT t.name, t.city, t.state FROM teams t
+       INNER JOIN leagues l
+       ON l.id = t.league_id
+       WHERE l.slug = ?",
+      [$slug]
+    );
 
-      return response()->json($teams);
-    }
+    return response()->json($teams);
+  }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,78 +6,78 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+      //
+  }
 
-    public function index()
-    {
-      $users = app('db')->select(
-        "SELECT id, name, email, first_name, last_name,
-          birthdate, city, state, zip FROM users");
+  public function index()
+  {
+    $users = app('db')->select(
+      "SELECT id, name, email, first_name, last_name,
+        birthdate, city, state, zip FROM users");
 
-      return response()->json($users);
-    }
+    return response()->json($users);
+  }
 
-    public function get($id)
-    {
-      $user = app('db')->select(
-        "SELECT id, name, email, first_name, last_name,
-          birthdate, city, state, zip FROM users WHERE id = ?",
-        [$id]
-      )[0];
+  public function get($id)
+  {
+    $user = app('db')->select(
+      "SELECT id, name, email, first_name, last_name,
+        birthdate, city, state, zip FROM users WHERE id = ?",
+      [$id]
+    )[0];
 
-      return response()->json($user);
-    }
+    return response()->json($user);
+  }
 
-    public function store(Request $request)
-    {
-      app('db')->insert(
-        "INSERT INTO users (name, email, password, first_name, last_name,
-          birthdate, city, state, zip, created_at, updated_at)
-         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        [
-          $request->input('name'),
-          $request->input('email'),
-          app('hash')->make($request->input('password')),
-          $request->input('first_name'),
-          $request->input('last_name'),
-          $request->input('birthdate'),
-          $request->input('city'),
-          $request->input('state'),
-          $request->input('zip'),
-          time(),
-          time()
-        ]
-      );
-    }
+  public function store(Request $request)
+  {
+    app('db')->insert(
+      "INSERT INTO users (name, email, password, first_name, last_name,
+        birthdate, city, state, zip, created_at, updated_at)
+       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        $request->input('name'),
+        $request->input('email'),
+        app('hash')->make($request->input('password')),
+        $request->input('first_name'),
+        $request->input('last_name'),
+        $request->input('birthdate'),
+        $request->input('city'),
+        $request->input('state'),
+        $request->input('zip'),
+        time(),
+        time()
+      ]
+    );
+  }
 
-    public function update(Request $request)
-    {
-      app('db')->update(
-        "UPDATE users
-         SET name = :name, email = :email, first_name = :first_name,
-             last_name = :last_name, birthdate = :birthdate, city = :city,
-             state = :state, zip = :zip, updated_at = :updated_at
-         WHERE id = :id",
-        [
-          'id' => $request->input('id'),
-          'name' => $request->input('name'),
-          'email' => $request->input('email'),
-          'first_name' => $request->input('first_name'),
-          'last_name' => $request->input('last_name'),
-          'birthdate' => $request->input('birthdate'),
-          'city' => $request->input('city'),
-          'state' => $request->input('state'),
-          'zip' => $request->input('zip'),
-          'updated_at' => time()
-        ]
-      );
-    }
+  public function update(Request $request)
+  {
+    app('db')->update(
+      "UPDATE users
+       SET name = :name, email = :email, first_name = :first_name,
+           last_name = :last_name, birthdate = :birthdate, city = :city,
+           state = :state, zip = :zip, updated_at = :updated_at
+       WHERE id = :id",
+      [
+        'id' => $request->input('id'),
+        'name' => $request->input('name'),
+        'email' => $request->input('email'),
+        'first_name' => $request->input('first_name'),
+        'last_name' => $request->input('last_name'),
+        'birthdate' => $request->input('birthdate'),
+        'city' => $request->input('city'),
+        'state' => $request->input('state'),
+        'zip' => $request->input('zip'),
+        'updated_at' => time()
+      ]
+    );
+  }
 }
